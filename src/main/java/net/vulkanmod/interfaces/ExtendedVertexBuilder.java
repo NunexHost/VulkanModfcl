048d78d30d6474e973830c4d3541f461ba8ae8fc
thr3343 <125277899+thr3343@users.noreply.github.com>
2024-01-05 19:55:23 +0000

    Missing Interface

--- src/main/java/net/vulkanmod/interfaces/ExtendedVertexBuilder.java
+++ src/main/java/net/vulkanmod/interfaces/ExtendedVertexBuilder.java
@@ -5,4 +5,7 @@ import net.vulkanmod.render.vertex.VertexUtil;
 public interface ExtendedVertexBuilder {
 
     void vertex(float x, float y, float z, int packedColor, float u, float v, int overlay, int light, int packedNormal);
+
+    //Particles
+    default void vertex(float x, float y, float z, float u, float v, int packedColor, int light) {}
 }
